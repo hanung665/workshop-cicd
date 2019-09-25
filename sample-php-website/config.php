a7d6f5567ff19825b362c09639ae4bfde37c1676
hanung665 <hanung665@gmail.com>
2019-09-25 12:40:58 +0700

    Update config.php

--- sample-php-website/config.php
+++ sample-php-website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website - 6',
+        'name' => 'Simple PHP Website - DEV-user20',
         'nav_menu' => [
             '' => 'Home',
             'about-us' => 'About Us',
